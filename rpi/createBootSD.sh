5f61a6a157accddfadebab4f6c61b836c1dad1d2
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-10-31 13:10:04 -0700

    better checking for raspian download

--- rpi/createBootSD.sh
+++ rpi/createBootSD.sh
@@ -16,7 +16,7 @@ function ensureDest() {
 
 function cleanUp() {
     ls "$dest_dir/"*.zip
-    ls "$dest_dir/"*.zip | wc -l
+    echo "`ls "$dest_dir/"*.zip | wc -l | xargs` zipped image(s)"
 }
 
 function getUriFilename() {
@@ -46,19 +46,34 @@ function ensureDownloaded() {
     fi
     
     # check if we already have a complete latest file
-    if [[ -f "$filename" ]] && unzip -tq "$filename" &>/dev/null; then 
-        echo "$filename is present and valid!"
+    if [[ -f "$filename" ]]; then
+        echo "Found $filename. Verifying structure..."
+        if unzip -tq "$filename" &>/dev/null; then 
+            echo "$filename is valid!"
+        else
+            echo "$filename is not complete. Resuming download..."
+            echo "Downloading $uri ==> $filename ..."
+            # Use -C - to resume the download
+            if ! curl -L $uri -C - -o "$filename" ; then
+                rm -v "$filename"
+                echo "Something was wrong with the download. Please try again."
+                return 1
+            else
+                echo "Finished download!"
+            fi
+            return
+        fi
     else
         echo "Downloading $uri ==> $filename ..."
         # Use -C - to resume the download
-        curl -L $uri -C - -o "$filename"
+        curl -L $uri -o "$filename"
     fi
     popd &>/dev/null
 }
 
 
 if [[ "$0" != "-bash" ]]; then
-    ensureDest
+    ensureDest &&
     ensureDownloaded
     cleanUp
 fi
